c2e44c038092a88b8b58a1063a96ff65be296898
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-09-28 19:07:17 +0000

    Started seperating files

--- Main.java
+++ Main.java
@@ -1,24 +1,4 @@
-import java.awt.*;
-import javax.swing.*;
 
-class Canvas extends JFrame {
-    Canvas() {
-        super("Canvas");
-        Canvas c = new Canvas() {
-            public void paint(Graphics g) {} //Used for initialization
-        };
-
-        c.setBackground(new Color(255, 255, 255));
-
-        add(c); //Adds the canvas to the JFrame
-        setSize(720, 720); //Should be a square room so why not a square JFrame
-        setVisible(true);
-
-
-
-
-    }
-}
 
 
 public class Main {
